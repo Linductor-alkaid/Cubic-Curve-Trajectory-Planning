6e093857534db5a0e5723074783dc8d8a8edcae0
Linductor-alkaid <202200171251@mail.sdu.edu.cn>
2024-12-02 21:04:20 +0800

    add CMakeLists

---
+++ build/CMakeFiles/CubicCurveTrajectoryPlanning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CubicCurveTrajectoryPlanning.dir/CubicCurveTrajectoryPlanning.cpp.o"
+  "CMakeFiles/CubicCurveTrajectoryPlanning.dir/main.cpp.o"
+  "CubicCurveTrajectoryPlanning"
+  "CubicCurveTrajectoryPlanning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CubicCurveTrajectoryPlanning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
